8552e07e4f246e6f2d8630cfde5f0c623889237c
John Schoeman <johnschoeman1617@gmail.com>
2022-02-19 13:00:32 -0500

    Update tailwind to 3.0
    This commit updates tailwind to v3.0 and sets the compile mode to `jit`.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.elm"],
-  darkMode: false, // or 'media' or 'class'
+  mode: "jit",
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.elm"],
+  darkMode: "media",
   theme: {
     extend: {},
   },
-  variants: {},
   plugins: [],
 };
